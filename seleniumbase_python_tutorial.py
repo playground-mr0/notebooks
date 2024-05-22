a9a8d59911086c09f1241f1b78be927ec8414b0e
playground-mr0 <playgroundmr0@gmail.com>
2024-05-22 11:42:16 +0200

    Add files via upload

---
+++ seleniumbase_python_tutorial.py
@@ -0,0 +1,46 @@
+#import time
+from seleniumbase import SB
+'''from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+with SB(uc=True) as sb:
+    
+    #=============================
+    #   Log in to Google account
+    #=============================
+    sb.open("https://www.google.com/gmail/about/")
+    sb.click('a[data-action="sign in"]')
+    sb.type('input[type="email"]', "playgroundmr0@gmail.com")
+    sb.click('button:contains("Next")')
+    sb.type('input[type="password"]', "X338LrDt$@:1")
+    sb.click('button:contains("Next")')
+    
+    time.sleep(10)
+    
+    #=============================
+    #   Open and run notebook
+    #=============================
+    sb.open("https://colab.research.google.com/drive/1MN8VKXladzXIRfzKYg-JNnH16iBfu7LC?usp=sharing")
+    
+    time.sleep(30)
+    
+    sb.click("body")
+    # Simulate Ctrl+F9
+    sb.send_keys("body", Keys.CONTROL + Keys.F9)
+    print("Pressed CTRL+F9 already")
+    time.sleep(30)
+    sb.send_keys("body", Keys.CONTROL + "s")
+    print("Pressed CTRL+S already")
+    time.sleep(10)
+    sb.send_keys("body", Keys.CONTROL + "e")
+    print("Pressed CTRL+E already")
+    print("Saving copy in Github with custom keyboard shortcut")
+    time.sleep(10)
+    sb.click("mwc-button[dialogaction='ok']")
+    input()
+    print("End of commands")
+
+    #while True:
+    #    pass
+    
+'''
